3d65ff94cae538082c60b0820a741341a67e1fb5
jiandankuanggong <74077677+1460293896@users.noreply.github.com>
2023-10-04 00:41:35 +0800

    Update configuration.h

--- xmrig/crypto/randomx/configuration.h
+++ xmrig/crypto/randomx/configuration.h
@@ -27,23 +27,23 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 
+
 #pragma once
 
 // Increase it if some configs use more cache accesses
-#define RANDOMX_CACHE_MAX_ACCESSES 8
+#define RANDOMX_CACHE_MAX_ACCESSES 16
 
 // Increase it if some configs use larger superscalar latency
 #define RANDOMX_SUPERSCALAR_MAX_LATENCY 256
 
 // Increase it if some configs use larger cache
-#define RANDOMX_CACHE_MAX_SIZE  33554368
+#define RANDOMX_CACHE_MAX_SIZE  268435456
 
 // Increase it if some configs use larger dataset
 #define RANDOMX_DATASET_MAX_SIZE  2181038080
 
 // Increase it if some configs use larger programs
-#define RANDOMX_PROGRAM_MAX_SIZE         256
+#define RANDOMX_PROGRAM_MAX_SIZE       280
 
 // Increase it if some configs use larger scratchpad
-#define RANDOMX_SCRATCHPAD_L3_MAX_SIZE      1048576
-
+#define RANDOMX_SCRATCHPAD_L3_MAX_SIZE      2097152
